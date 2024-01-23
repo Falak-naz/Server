f562f4d069e075008942dcc30efb100ecd165838
Demo <falkunaz@gmail.com>
2024-01-23 15:15:01 +0500

    validations done

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,24 +1,29 @@
 import { UserModel } from "../models/user.model.js";
 import { UserService } from "../services/index.js";
 import { httpResponse } from "../utils/index.js";
-import  passwordHash from "password-hash";
+import passwordHash from "password-hash";
+import jwt from "jsonwebtoken";
+import { config } from "dotenv";
 
 export const UserController = {
-
   signup: async (req, res) => {
     const { name, email, password } = req.body;
+
     try {
       console.log(name, email, password);
-      const userExist = await UserService.emailExist(email); 
+      const userExist = await UserService.emailExist(email);
 
       if (userExist) {
-        return httpResponse.CONFLICT(res, { error: "Email already exists" }); 
+        return httpResponse.CONFLICT(res, { error: "Email already exists" });
       }
 
-       
-      const hashpassword =  passwordHash.generate(password);
-      
-      const data = await UserService.signup({ name, email, password:hashpassword});
+      const hashpassword = passwordHash.generate(password);
+
+      const data = await UserService.signup({
+        name,
+        email,
+        password: hashpassword,
+      });
       if (data) {
         return httpResponse.SUCCESS(res, data);
       }
@@ -29,11 +34,24 @@ export const UserController = {
 
   signin: async (req, res) => {
     try {
-      const {email} = req.body;
-        const data = await UserService.emailCheck(email); // check email id is already available or not
-        if(data){
-          return httpResponse.SUCCESS(res, data);
-        }
+      const { email, password } = req.body;
+
+      const data = await UserService.emailCheck(email); // check email id is already available or not
+      if (!data) {
+        return httpResponse.NOT_FOUND(res, { error: "user not found" });
+      }
+      const hashedPassword = passwordHash.verify(password, data.password);
+
+      if (!hashedPassword) {
+        return httpResponse.NOT_FOUND(res, { error: "password not matched" });
+      }
+
+      const token = jwt.sign({ userId: data._id }, process.env.JWT_SECRET, {
+        expiresIn: "1h",
+      });
+      if (data) {
+        return httpResponse.SUCCESS(res, data, { token: token });
+      }
     } catch (error) {
       return httpResponse.INTERNAL_SERVER_ERROR(res, error);
     }
