67f0825a38a39920afdec0e2212bb929d4ab0df0
Demo <falkunaz@gmail.com>
2024-02-15 12:42:35 +0500

    issue resolved

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -3,7 +3,7 @@ import { UserValidationSchema } from "../validations/index.js";
 import { validate, authenticate } from "../middleware/index.js";
 import { UserController } from "../controllers/index.js";
 
-
+const router = express.Router();
 router.post("/signup",validate(UserValidationSchema.add), UserController.signup)
 router.post("/signin", UserController.signin)
 router.put('/about/:id', upload.single('profilePic'), validate(UserValidationSchema.update), UserController.updateUser);
